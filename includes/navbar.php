e65e168aa4082a53cacbc0c8af1f9ebfc64ae62f
HeyAnto <antonin63170@gmail.com>
2025-01-24 11:15:19 +0100

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar">
+        <a class="navbar-brand" href="/index.php">
+            <img class="logo" src="/assets/logo.svg">
+        </a>
+        <a type="button" href="/pages/contact.php" class="btn-primary">
+            Contact
+        </a>
+    </nav>
+</header>
---
+++ pages/contact.php
@@ -0,0 +1,11 @@
+<?php
+include_once "../includes/header.php";
+?>
+
+<main>
+    <h1>Contact</h1>
+</main>
+
+<?php
+include_once "../includes/footer.php";
+?>

0b37e026c63f04cca69f86e81f843f50a22070cd
HeyAnto <antonin63170@gmail.com>
2025-01-28 11:51:58 +0100

    Update Form

--- pages/form.php
+++ pages/form.php
@@ -4,12 +4,17 @@
     <div class="container-fluid" style="max-width: 800px;">
         <h1>Données du formulaire</h1>
         <?php if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["subject"]) && isset($_POST["message"])): ?>
+            <div class="alert alert-success text-center" role="alert">
+                Votre message a été envoyé avec succès !
+            </div>
             <p><b>Nom :</b> <?php echo $_POST["name"] ?> </p>
             <p><b>Email :</b> <?php echo $_POST["email"] ?> </p>
             <p><b>Sujet :</b> <?php echo $_POST["subject"] ?> </p>
             <p class="message-box"><b>Message :</b> <?php echo $_POST["message"] ?> </p>
         <?php else: ?>
-            <p><b>Aucune données trouver.</b></p>
+            <div class="alert alert-danger text-center" role="alert">
+                Aucune données trouver.
+            </div>
         <?php endif; ?>
     </div>
 </main>
